0d90ea1b3684939f0429fd58bcaea3da8efc5545
Yemi <olayemey@gmail.com>
2021-12-17 12:53:36 +0100

    carts project

---
+++ script.js
@@ -0,0 +1,70 @@
+//alert('yes');
+window.addEventListener('load', function(){
+	let cartCounter = document.querySelector("#cart-counter");
+	let storedCartItems = getStoredCartItems();
+
+	if(storedCartItems.length == 0){
+		cartCounter.innerText = "0";
+	}else{
+		cartCounter.innerText = `${storedCartItems.length}`;
+	}
+
+});
+
+
+
+const btns = document.querySelectorAll('#btns');
+//console.log(btns);	
+
+	/*let dad = btns.parentNode;
+	let all = dad.childNodes;
+	let name = dad.childNodes[3].innerText;
+	console.log(dad);
+	console.log(name);
+	let imgSrc = dad.childNodes[1].src;	*/
+	
+
+btns.forEach((btn) => {
+
+	btn.addEventListener('click', function(){
+
+	let dad = this.parentNode;
+	let productName = dad.childNodes[3].innerText;
+	let imgSrc = dad.childNodes[1].src;
+
+		
+	
+		let product = {
+			'product_name' : productName, 
+			'product_image' : imgSrc
+		}
+
+
+		let storedCartItems = getStoredCartItems();
+
+
+		storedCartItems.push(product);
+		storedCartItems = JSON.stringify(storedCartItems);
+
+		localStorage.setItem('cartBank', storedCartItems);
+
+		location.reload();
+
+
+
+	})
+
+}) 
+
+
+function getStoredCartItems(){
+	let storedCartItems = localStorage.getItem("cartBank");
+
+	if(storedCartItems == null || storedCartItems == undefined){
+		return [];
+	}else{
+		
+		storedCartItems = JSON.parse(storedCartItems);
+		return storedCartItems;
+	}
+}
